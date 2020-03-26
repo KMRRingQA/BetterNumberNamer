de1d8c0c69a54715927f3d6d69665a992cead703
Korbinian <kmr.ring@outlook.com>
2020-03-26 12:55:42 +0000

    first commit.

---
+++ src/com/qa/main/BetterNumberNamer.java
@@ -0,0 +1,71 @@
+package com.qa.main;
+
+public class BetterNumberNamer {
+
+	final String[] unitName = { "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+			"eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+
+	final String[] deciName = { "", "", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+
+	final String[] powersName = { "", "", "", "thousand", "thousand", "thousand", "million", "million", "million",
+			"billion", "billion", "billion", "trillion", "trillion", "trillion" };
+
+	int howLong(int Num) {
+		int length = 0;
+		for (int i = 1; i <= Num; i *= 10) {
+			length++;
+		}
+		return length;
+
+	}
+
+	void nameThatNumber(int[] Split, int length) {
+		int DigitMultiplier = length / 3 + 1;
+		while (DigitMultiplier > 0) {
+			int units = 3 * DigitMultiplier - 3;
+			int tens = 3 * DigitMultiplier - 2;
+			int hundreds = 3 * DigitMultiplier - 1;
+			int skip = 0;
+			if (Split[tens] == 0 && Split[units] == 0 && Split[hundreds] == 0) {
+				skip = 1;
+			} else if (Split[hundreds] == 0) {
+				if (Split[tens] < 2) {
+					System.out.print(unitName[Split[tens] * 10 + Split[units]]);
+				} else if (Split[units] == 0) {
+					System.out.print(deciName[Split[tens]]);
+				} else {
+					System.out.print(deciName[Split[tens]] + "-" + unitName[Split[units]]);
+				}
+			} else if (Split[tens] == 0 && Split[units] == 0) {
+				System.out.print(unitName[Split[hundreds]] + " hundred");
+			} else if (Split[tens] < 2) {
+				System.out
+						.print(unitName[Split[hundreds]] + " hundred and " + unitName[Split[tens] * 10 + Split[units]]);
+			} else if (Split[units] == 0) {
+				System.out.print(unitName[Split[hundreds]] + " hundred and " + deciName[Split[tens]]);
+			} else {
+				System.out.print(unitName[Split[hundreds]] + " hundred and " + deciName[Split[tens]] + "-"
+						+ unitName[Split[units]]);
+			}
+			if (length >= 3 && skip == 0) {
+				System.out.print(" " + powersName[length] + " ");
+			}
+			DigitMultiplier--;
+			length -= 3;
+		}
+	}
+
+	int[] splitIntoArray(int Num, int length) {
+		int[] Split = new int[length + 3];
+
+		for (int i = 0; i < length; i++) {
+			int digit = (int) ((Num / (Math.pow(10, i))) % 10);
+			Split[i] = digit;
+		}
+		for (int i = length; i < length + 3; i++) {
+			Split[i] = 0;
+		}
+		return Split;
+	}
+
+}
---
+++ src/com/qa/main/Runner.java
@@ -0,0 +1,21 @@
+package com.qa.main;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		BetterNumberNamer test = new BetterNumberNamer();
+		int Num = 1860003566;
+		int length = (test.howLong(Num));
+//		System.out.println(length + "\n");
+		int[] Split = test.splitIntoArray(Num, length);
+
+//		for (int i = length - 1; i >= 0; i--) {
+//			System.out.println(Split[i]);
+//		}
+//		for (int i = 0; i < length + 3; i++) {
+//			System.out.println(Split[i]);
+//		}
+
+		test.nameThatNumber(Split, length);
+	}
+}
